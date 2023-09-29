c01fe5618aaafffc143680a27680956f2aa533de
pierrick <pierrickviret@gmail.com>
2023-09-29 06:32:12 +0200

    add list of ingredient View

---
+++ Reciplease/Common/UITextView+.swift
@@ -0,0 +1,21 @@
+//
+//  UITextView+.swift
+//  Reciplease
+//
+//  Created by pierrick viret on 29/09/2023.
+//
+
+import Foundation
+import UIKit
+
+extension UITextView {
+    func setupDynamicTextWith( policeName: String, size: CGFloat, style: UIFont.TextStyle) {
+        if let font = UIFont(name: policeName, size: size) {
+            let fontMetrics = UIFontMetrics(forTextStyle: style)
+            self.font = fontMetrics.scaledFont(for: font)
+        }
+        self.adjustsFontForContentSizeCategory = true
+    }
+
+    
+}
--- Reciplease/SearchController.swift
+++ Reciplease/SearchController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class SearchController: UIViewController {
     //
-
     let searchArea: UIView = {
         let view = UIView()
         view.backgroundColor = .white
@@ -34,6 +33,14 @@ class SearchController: UIViewController {
         return label
     }()
 
+    let inputStackView: UIStackView = {
+        let view = UIStackView()
+        view.backgroundColor = .white
+        view.axis = .horizontal
+        view.distribution = .fill
+        return view
+    }()
+
     let addIngredients: UIButton = {
         let button = UIButton()
         button.setTitle("Add", for: .normal)
@@ -49,7 +56,7 @@ class SearchController: UIViewController {
        let textField = UITextField()
         textField.placeholder = "Lemon, Cheese..."
         textField.setupDynamicTextWith(policeName: "Symbol", size: 25, style: .title3)
-        textField.textAlignment = .justified
+        textField.textAlignment = .left
         textField.textColor = .darkGray
         textField.setAccessibility(with: .searchField, label: "input Ingredients", hint: "Area to input your ingredient")
         return textField
@@ -61,12 +68,42 @@ class SearchController: UIViewController {
         return view
     }()
 
-    let inputStackView: UIStackView = {
-        let view = UIStackView()
-        view.backgroundColor = .white
-        view.axis = .horizontal
-        view.distribution = .fillProportionally
-        return view
+    let clearStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        return stackView
+    }()
+
+    let yourIngredientsLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Your ingredients :"
+        label.setupDynamicTextWith(policeName: "Chalkduster", size: 23, style: .largeTitle)
+        label.textColor = .white
+        label.textAlignment = .left
+        return label
+    }()
+
+    let clearIngredients: UIButton = {
+        let button = UIButton()
+        button.setTitle(" Clear ", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.numberOfLines = 1
+        button.backgroundColor = .darkGray
+        button.setupDynamicTextWith(style: .body)
+        button.layer.cornerRadius = 5
+        button.setAccessibility(with: .button, label: "clear ingredient", hint: "Pressed button to clear ingredient")
+        return button
+    }()
+
+    let ingredientList: UITextView = {
+       let texte = UITextView()
+        texte.textColor = .white
+        texte.backgroundColor = .anthraciteGray
+        texte.isEditable = false
+        texte.setupDynamicTextWith(policeName: "Chalkduster", size: 23, style: .largeTitle)
+        texte.setAccessibility(with: .staticText, label: "ingredients", hint: "Ingredient in your liste")
+        return texte
     }()
 
     let searchRecipes: UIButton = {
@@ -91,6 +128,16 @@ class SearchController: UIViewController {
         navigationController?.navigationBar.topItem?.title = "Reciplease"
 
         setupView()
+        ingredientList.text = 
+        """
+            - Tomato
+
+            - Avocado
+
+            - cheese
+
+            - banana
+        """
     }
 
     // MARK: - Layout
@@ -98,6 +145,11 @@ class SearchController: UIViewController {
         [searchStackView, questionTitle, addIngredients, inputIngredients, underligne, inputStackView, searchRecipes, searchArea].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
+
+        [clearStackView, yourIngredientsLabel, clearIngredients, ingredientList].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
         view.addSubview(searchArea)
         NSLayoutConstraint.activate([
             searchArea.leftAnchor.constraint(equalTo: view.leftAnchor),
@@ -108,8 +160,8 @@ class SearchController: UIViewController {
 
         searchArea.addSubview(searchStackView)
         NSLayoutConstraint.activate([
-            searchStackView.leftAnchor.constraint(equalTo: searchArea.leftAnchor, constant: 5),
-            searchStackView.rightAnchor.constraint(equalTo: searchArea.rightAnchor, constant: -5),
+            searchStackView.leftAnchor.constraint(equalTo: searchArea.leftAnchor, constant: 10),
+            searchStackView.rightAnchor.constraint(equalTo: searchArea.rightAnchor, constant: -10),
             searchStackView.topAnchor.constraint(equalTo: searchArea.topAnchor, constant: 10),
             searchArea.bottomAnchor.constraint(equalTo: searchStackView.bottomAnchor, constant: 15)
         ])
@@ -128,12 +180,31 @@ class SearchController: UIViewController {
             underligne.topAnchor.constraint(equalTo: inputIngredients.bottomAnchor, constant: 5)
         ])
 
+        view.addSubview(clearStackView)
+        clearStackView.addArrangedSubview(yourIngredientsLabel)
+        clearStackView.addArrangedSubview(clearIngredients)
+        NSLayoutConstraint.activate([
+            clearStackView.topAnchor.constraint(equalTo: searchArea.bottomAnchor, constant: 10),
+            clearStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            clearStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            addIngredients.widthAnchor.constraint(equalTo: clearIngredients.widthAnchor),
+            addIngredients.heightAnchor.constraint(equalTo: clearIngredients.heightAnchor)
+        ])
+
         view.addSubview(searchRecipes)
         NSLayoutConstraint.activate([
             searchRecipes.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             searchRecipes.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
             searchRecipes.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
             searchRecipes.heightAnchor.constraint(greaterThanOrEqualToConstant: 60)
+        ])
+
+        view.addSubview(ingredientList)
+        NSLayoutConstraint.activate([
+            ingredientList.topAnchor.constraint(equalTo: clearStackView.bottomAnchor, constant: 10),
+            ingredientList.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            ingredientList.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            ingredientList.bottomAnchor.constraint(equalTo: searchRecipes.topAnchor, constant: -10)
         ])
     }
 }
@@ -155,11 +226,15 @@ extension SearchController {
             inputIngredients.placeholder = "ingredient"
             searchRecipes.setTitle("Search", for: .normal)
             questionTitle.isHidden = true
+            underligne.isHidden = true
+            yourIngredientsLabel.isHidden = true
         } else {
             inputStackView.axis = .horizontal
             inputIngredients.placeholder = "Lemon, Cheese..."
             searchRecipes.setTitle("Search for recipes", for: .normal)
             questionTitle.isHidden = false
+            underligne.isHidden = false
+            yourIngredientsLabel.isHidden = false
         }
     }
 }
