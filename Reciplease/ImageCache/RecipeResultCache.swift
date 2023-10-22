dd9702f9192c1ea60622a83eb7b987bee8cddff8
pierrick <pierrickviret@gmail.com>
2023-10-22 14:50:40 +0200

    add cache for search Recipes

---
+++ Reciplease/ImageCache/RecipeResultCache.swift
@@ -0,0 +1,33 @@
+//
+//  recipeResultCache.swift
+//  Reciplease
+//
+//  Created by pierrick viret on 22/10/2023.
+//
+
+import Foundation
+import UIKit
+
+class RecipeResultCache {
+
+    // MARK: - Singleton
+    static let shared = RecipeResultCache()
+
+    // MARK: - Propertie
+    private let recipeCache = NSCache<NSString, NSArray>()
+
+    // MARK: - INIT
+   private init() {
+       recipeCache.countLimit = 50
+       recipeCache.totalCostLimit = 50 * 1024 * 1024
+    }
+
+    // MARK: - Function
+    func save(recipes: [LocalRecipe], forKey ingredients: String) {
+        self.recipeCache.setObject(recipes as NSArray, forKey: ingredients as NSString)
+    }
+
+    func getRecipes(for ingredients: String) -> [LocalRecipe] {
+        return (recipeCache.object(forKey: ingredients as NSString) as? [LocalRecipe]) ?? []
+    }
+}
--- Reciplease/ResultSearch/ResultSearchViewModel.swift
+++ Reciplease/ResultSearch/ResultSearchViewModel.swift
@@ -14,43 +14,59 @@ class ResultSearchViewModel: ObservableObject {
     var cancellables = Set<AnyCancellable>()
 
     private let repository = Repository()
-    private let recipesSaveRepository = RecipeSaveManager()
+    private let recipesSaveManager = RecipeSaveManager()
     var recipes: [LocalRecipe] = []
+    private var ingredientsKey: String!
 
     @Published var isNetworkSuccessful: Bool!
     @Published var isAlerte: ErrorMessage!
 
     func fetchInitRecipes(with ingredients: [String]) {
+        self.ingredientsKey = ingredients.joined(separator: ",")
+        if haveRecipesInCache() {
+            checkIfRecipesCanBeShow()
+        } else {
+            fetchRecipes(with: ingredients)
+        }
+    }
+
+    private func fetchRecipes(with ingredients: [String]) {
         self.isNetworkSuccessful = false
 
         repository.fetchRecip(ingredients)
-            .sink { completion in
-            switch completion {
-            case .finished:
-                break
-            case .failure:
-                self.isAlerte = ErrorMessage(title: "Erreur", message: "Erreur de reseau")
-            }
-        } receiveValue: { [weak self] data in
-            guard let self = self else { return }
+            .sink { [weak self] completion in
+                guard let self = self else { return }
 
-            data.hits.forEach { hit in
-                self.recipes.append(LocalRecipe(recipe: hit.recipe))
-            }
-            checkRecipes()
-        }.store(in: &cancellables)
+                switch completion {
+                case .finished:
+                    break
+                case .failure:
+                    self.isAlerte = ErrorMessage(title: "Erreur", message: "Erreur de reseau")
+                }
+            } receiveValue: { [weak self] data in
+                guard let self = self else { return }
+
+                self.recipes = data.hits.map { LocalRecipe(recipe: $0.recipe) }
+                self.checkIfRecipesCanBeShow()
+            }.store(in: &cancellables)
     }
 
     func fetchSaveRecipes() {
-        recipes = recipesSaveRepository.fetchRecipes()
-        checkRecipes()
+        recipes = recipesSaveManager.fetchRecipes()
+        checkIfRecipesCanBeShow()
     }
 
-    private func checkRecipes() {
+    private func checkIfRecipesCanBeShow() {
         if self.recipes.isEmpty == true {
             self.isAlerte = ErrorMessage(title: "Information", message: "Pas de recette disponible")
         } else {
+            RecipeResultCache.shared.save(recipes: recipes, forKey: ingredientsKey)
             self.isNetworkSuccessful = true
         }
+    }
+
+    private func haveRecipesInCache() -> Bool {
+        recipes = RecipeResultCache.shared.getRecipes(for: ingredientsKey)
+        return !recipes.isEmpty
     }
 }
